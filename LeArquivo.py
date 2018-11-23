3f64e8807a0ea9e692d7d1bdcc8ac93210538602
Guilherme Sant'Anna <guilherme1230@microblau.com.br>
2018-11-23 11:34:04 -0200

    Faz a leitura dos arquivos Excel  e retorna os dados não nulos

--- LeArquivo.py
+++ LeArquivo.py
@@ -1,2 +1,15 @@
 #Import de arquivos
 import pandas
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw()
+filename = askopenfilename()
+
+# Carrega os Dados
+url = pandas.ExcelFile(filename)
+names = ['POINT-INDEX', 'EQUIPAMENTO', 'IO']
+dataset = pandas.read_excel(url, sheet_name='Configuração', names=names, usecols='H,I,J')
+datasets = dataset.drop(dataset[dataset['POINT-INDEX'] == 0].index)
+print(datasets)
