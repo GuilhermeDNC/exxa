4115cc4131dd0776005cc63bb8dd1f99e75022bd
Guilherme Sant'Anna <guilherme1230@microblau.com.br>
2018-11-23 14:19:34 -0200

    Organização das entradas

--- LeArquivo.py
+++ LeArquivo.py
@@ -12,4 +12,14 @@ url = pandas.ExcelFile(filename)
 names = ['POINT-INDEX', 'EQUIPAMENTO', 'IO']
 dataset = pandas.read_excel(url, sheet_name='Configuração', names=names, usecols='H,I,J')
 datasets = dataset.drop(dataset[dataset['POINT-INDEX'] == 0].index)
-print(datasets)
+
+pointIndex = datasets['POINT-INDEX'].values.tolist()
+
+equipamento = datasets['EQUIPAMENTO'].values.tolist()
+
+io = datasets['IO'].values.tolist()
+
+print("\tpublic void configura(){")
+for i in range(0, len(pointIndex)):
+    print("\t\t{}.{}.pointIndex = {};".format(equipamento[i],io[i],pointIndex[i]))
+print("\t}")
