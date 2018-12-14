014e1995511e730bb4d0d778327ca96decdb8e40
Guilherme Sant'Anna <guilherme1230@microblau.com.br>
2018-12-14 13:11:11 -0200

    Inclusão do dicionário

--- LeArquivo.py
+++ LeArquivo.py
@@ -1,25 +1,44 @@
 #Import de arquivos
 import pandas
+import Dicionario as dc
 
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 Tk().withdraw()
-filename = askopenfilename()
+filename = askopenfilename(initialdir = "/",title = "Abrir Tabela de Variáveis",filetypes = (("Arquivo Excel","*.xls"),("Todos os Arquivos","*.*")))
 
 # Carrega os Dados
 url = pandas.ExcelFile(filename)
-names = ['POINT-INDEX', 'EQUIPAMENTO', 'IO']
-dataset = pandas.read_excel(url, sheet_name='Configuração', names=names, usecols='H,I,J')
-datasets = dataset.drop(dataset[dataset['POINT-INDEX'] == 0].index)
+names = ['PASTA', 'DESCRIÇÃO', 'IO']
 
-pointIndex = datasets['POINT-INDEX'].values.tolist()
+dataset = pandas.read_excel(url, sheet_name='Configuração', names=names, usecols='A,E,F',skiprows=12)
+datasets = dataset[dataset['IO'].notnull()]
 
-equipamento = datasets['EQUIPAMENTO'].values.tolist()
+pointIndex = datasets['IO'].values.tolist()
+equipamento = datasets['PASTA'].values.tolist()
+io = datasets['DESCRIÇÃO'].values.tolist()
 
-io = datasets['IO'].values.tolist()
+metodo = ''
+metodo += '\tpublic void pointIndex(){\n'
 
-print("\tpublic void configura(){")
 for i in range(0, len(pointIndex)):
-    print("\t\t{}.{}.pointIndex = {};".format(equipamento[i],io[i],pointIndex[i]))
-print("\t}")
+    metodo += ('\t\t'+dc.pasta[equipamento[i]]+dc.descricao[io[i]]+dc.io[pointIndex[i]] + ';\n')
+metodo += '\t}\n'
+
+sistemaPath = askopenfilename(initialdir = "C:\Java Controls",title = "Abrir arquivo Sistema.java",filetypes = (("Arquivo Java","*.java"),("Todos os Arquivos","*.*")))
+
+f = open(sistemaPath, "r")
+contents = f.readlines()
+f.close()
+
+linhas = sum(1 for linha in contents)
+
+contents.insert(linhas-1, metodo)
+
+f = open(sistemaPath, "w")
+contents = "".join(contents)
+f.write(contents)
+f.close()
+
+
